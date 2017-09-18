00b959b9a575dbf4d2fbff35c9c61cc9f011856f
Jaga Santagostino <jagasantagostino@gmail.com>
2017-09-19 00:27:24 +0200

    better neotree beybind

--- init.el
+++ init.el
@@ -541,7 +541,7 @@ IF TESTNAME is specified run jest with a pattern for just that test."
 (global-set-key (kbd "s-/") 'comment-line)
 (global-set-key (kbd "s-D") 'spacemacs/duplicate-line-or-region)
 (global-set-key (kbd "s-p") 'helm-projectile-find-file)
-(global-set-key (kbd "s-1") 'neotree-toggle)
+(global-set-key (kbd "s-1") 'neotree-find-project-root)
 (global-set-key (kbd "s-3") 'spacemacs/default-pop-shell)
 (global-set-key (kbd "C-c C-j") 'ace-jump-mode)
 (global-set-key (kbd "C-c C-t") 'emmet-wrap-with-markup)
