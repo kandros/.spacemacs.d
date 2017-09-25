258a2c309d0ba7b1829c2ebdfc1e3265094e3d6e
Jaga Santagostino <jagasantagostino@gmail.com>
2017-09-25 18:21:44 +0200

    remove eyebrowse conflicting with org-mode

--- init.el
+++ init.el
@@ -92,6 +92,7 @@
    dotspacemacs-excluded-packages '(
                                     tern
                                     company-tern
+                                    eyebrowse
                                     )
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
