d3e28ea0bd0cb5740343cd0d28ef7cce6dc47ab6
Jaga Santagostino <jagasantagostino@gmail.com>
2017-09-25 18:23:17 +0200

    fix documentation typo

--- init.el
+++ init.el
@@ -668,7 +668,7 @@ IF TESTNAME is specified run jest with a pattern for just that test."
 (evil-define-key 'visual global-map
   (kbd "`") 'wrap-with-backtick)
 
-;; make copy when a region is selected like if should (replace text with system clipobard)
+;; make paste when a region is selected like if should (replace text with system clipobard)
 ;; https://emacs.stackexchange.com/questions/14940/emacs-doesnt-paste-in-evils-visual-mode-with-every-os-clipboard/15054#15054
 (fset 'evil-visual-update-x-selection 'ignore)
 (delete-selection-mode 1)
